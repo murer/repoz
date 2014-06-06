d78baa12f9dcfd083e83fb7f863913bc0a5aaa55
murer <murer@dextra-sw.com>
2014-06-06 14:00:25 -0300

    init

---
+++ repoz-web/src/main/java/com/murerz/repoz/web/meta/FileAccessManager.java
@@ -0,0 +1,13 @@
+package com.murerz.repoz.web.meta;
+
+public class FileAccessManager implements AccessManager {
+
+	public boolean authenticate(String username, String password) {
+		return false;
+	}
+
+	public boolean authorize(String username, String path) {
+		return false;
+	}
+
+}
