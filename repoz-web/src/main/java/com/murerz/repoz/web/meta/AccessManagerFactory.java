7874ec7c9202c1488b796d53bd20f1fe0ffc5eeb
murer <murer@dextra-sw.com>
2014-06-06 14:00:16 -0300

    init

--- repoz-web/src/main/java/com/murerz/repoz/web/meta/AccessManagerFactory.java
+++ repoz-web/src/main/java/com/murerz/repoz/web/meta/AccessManagerFactory.java
@@ -7,7 +7,7 @@ public class AccessManagerFactory {
 	public static final String PROPERTY = "repoz.accessmanager.impl";
 
 	public static AccessManager create() {
-		String className = System.getProperty(PROPERTY, GrantAccessManager.class.getName());
+		String className = System.getProperty(PROPERTY, FileAccessManager.class.getName());
 		return (AccessManager) ReflectionUtil.newInstance(className);
 	}
 
