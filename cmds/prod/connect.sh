c8b49525491ef65dceb04d54f1901351ade97154
murer <fuweweu@gmail.com>
2014-06-20 08:16:14 -0300

    de casa

--- cmds/prod/connect.sh
+++ cmds/prod/connect.sh
@@ -5,4 +5,4 @@ EXT_IP=108.59.84.254
 #	EXT_IP="$(gcutil --project docs-manager listinstances  --zone us-central1-a  --filter="name eq 'repoz-prod'" | cut -d"|" -f6 | tail -n +4 | head -n 1| sed "s/\s\+//g")"
 #done
 
-ssh -o ConnectTimeout=15 -o UserKnownHostsFile=/dev/null -o CheckHostIP=no -o StrictHostKeyChecking=no -i gen/prod/id_rsa repoz@$EXT_IP $*
+ssh -o ConnectTimeout=15 -o UserKnownHostsFile=/dev/null -o CheckHostIP=no -o StrictHostKeyChecking=no repoz@$EXT_IP $*
--- cmds/prod/repoz-install/repoz-update.sh
+++ cmds/prod/repoz-install/repoz-update.sh
@@ -5,6 +5,7 @@ source /etc/bash.bashrc.repoz
 cat > packs/repoz.war
 
 cp -v repoz-repo/repoz.properties packs/WEB-INF/classes/repoz.properties 
+cp -v repoz-repo/gcs.p12 packs/WEB-INF/classes/gcs.p12
 
 cd packs
 
