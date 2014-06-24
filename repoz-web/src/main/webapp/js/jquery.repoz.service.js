c1ad24111683aefeb6ffddb043303a7593a10b75
murer <murer@dextra-sw.com>
2014-06-24 10:33:40 -0300

    init

--- repoz-web/src/main/webapp/js/jquery.repoz.service.js
+++ repoz-web/src/main/webapp/js/jquery.repoz.service.js
@@ -14,19 +14,19 @@
 
 	$.repoz.parseTextLine = function(row) {
 		row = $.trim(row);
-		if(!row || row.match(/^#/)) {
+		if (!row || row.match(/^#/)) {
 			return null;
 		}
 		var list = row.split(/\s+/);
 		return list;
 	}
-	
+
 	$.repoz.parseText = function(text) {
 		var list = text.split('\n');
 		var ret = [];
-		for(var i = 0; i < list.length; i++) {
+		for (var i = 0; i < list.length; i++) {
 			var row = $.trim(list[i]);
-			if(row && !row.match(/^#/)) {
+			if (row && !row.match(/^#/)) {
 				var line = $.repoz.parseTextLine(row);
 				ret.push(line);
 			}
@@ -40,11 +40,31 @@
 		}, function(file) {
 			var matrix = $.repoz.parseText(file);
 			var list = [];
-			for(var i = 0; i < matrix.length; i++) {
+			for (var i = 0; i < matrix.length; i++) {
 				list.push(matrix[i][0]);
 			}
 			c(list);
-			
+
+		});
+	}
+
+	$.repoz.listRepoUsers = function(path, c) {
+		$.repoz.service('GET', 'access', null, {
+			path : path
+		}, function(file) {
+			var matrix = $.repoz.parseText(file);
+			var list = [];
+			for (var i = 0; i < matrix.length; i++) {
+				var row = matrix[i];
+				list.push({
+					repo : row[0],
+					user : row[1],
+					type : row[2],
+					pass : row[3]
+				});
+			}
+			c(list);
+
 		});
 	}
 
