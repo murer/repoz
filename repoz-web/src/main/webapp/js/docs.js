0cd65cac570d0857fbe93cd8a431f96d92caafc4
murer <murer@dextra-sw.com>
2014-06-25 11:01:27 -0300

    x

--- repoz-web/src/main/webapp/js/docs.js
+++ repoz-web/src/main/webapp/js/docs.js
@@ -1,3 +1,11 @@
 (function($) {
 
+	var baseurl = '' + location;
+	baseurl = baseurl.replace(/docs\.html.*/g, '');
+	console.info(baseurl);
+	
+	$(window).ready(function() {
+		$('.code.url').text(baseurl + 'r/');
+	});
+	
 })(jQuery);
