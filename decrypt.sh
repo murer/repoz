71ec9beb4799dbff6f5dfa6cd73a675c79b8be2d
murer <murer@dextra-sw.com>
2014-07-02 13:45:56 -0300

    x

--- decrypt.sh
+++ decrypt.sh
@@ -7,6 +7,6 @@ fi
 
 find -name "*.repoz-crypt" | while read k; do 
 	echo "decrypt: $k";
-	openssl enc -des-ede3-cbc -salt -in "$k" -out "$(echo "$k" | sed "s/\.repoz-crypt$//g")" -d -pass "pass:$REPOZSECRET"; 
+	openssl enc -aes-256-cbc -salt -in "$k" -out "$(echo "$k" | sed "s/\.repoz-crypt$//g")" -d -pass "pass:$REPOZSECRET"; 
 done
 
--- encrypt-file.sh
+++ encrypt-file.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-openssl enc -des-ede3-cbc -salt -in "$1" -out "$1.repoz-crypt" -pass "pass:$REPOZSECRET";
+openssl enc -aes-256-cbc -salt -in "$1" -out "$1.repoz-crypt" -pass "pass:$REPOZSECRET";
 
