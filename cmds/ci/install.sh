89c8a32196ac7df00cffa69c0d37d32afe1d92a9
murer <murer@dextra-sw.com>
2014-07-07 13:35:39 -0300

    x

---
+++ cmds/ci/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if ! mvn clean -B -Dmaven.test.skip.exec -T 10; then
+	if ! mvn clean -B -Dmaven.test.skip.exec -T 10; then
+		mvn clean -B -Dmaven.test.skip.exec -T 10;
+	fi;
+fi
+
+if ! mvn install -B -Dmaven.test.skip.exec -T 10; then
+	if ! mvn install -B -Dmaven.test.skip.exec -T 10; then
+		mvn install -B -Dmaven.test.skip.exec -T 10;
+	fi;
+fi
+
