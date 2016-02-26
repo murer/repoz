3e2ad2140f997144851f8a0b077a828e0598d333
murer <murer@dextra-sw.com>
2016-02-26 18:23:31 -0300

    docker

--- cmds/prod/repoz-install/install-instance.sh
+++ cmds/prod/repoz-install/install-instance.sh
@@ -9,6 +9,7 @@ adduser --disabled-password --gecos "" repoz
 mkdir /home/repoz/.ssh || true
 ssh-keygen -t rsa -N "" -f /home/repoz/.ssh/id_rsa
 cp /home/repoz/.ssh/id_rsa.pub /home/repoz/.ssh/authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCkVvGeF0Q1aN2hwsmxUnJftBqd1JNGwkkQcyKOAi0lD1c9+GxGFDetG5OJCQk7ZX5cA53430vqu8GEJrPxrwmOtY09KxB+XQXUePmuy4gAACfulFVn/G6fvUsApzh5uevTequHFPMJ6A6uskAgAKH3RvgbrLj21r8kTpvOpb/3tEJKGfxtV9xQIB0GHhRACqGgswo0gXJvR0MZkd7AIQQUxaHfPye/u7fs06IBQrw8q1GrpztU9HNis2BJewBMNzFx28b1v/6RI2wv/5RVoVPgMpZJ48kVGy0uKnMEmNfAN+hFVZ5pKe8PZ9ti2zBZyhEoU/toWD8pd3Yvpmt4fRwL repoz@ci" >> /home/repoz/.ssh/authorized_keys
 
 cd /home/repoz
 
--- cmds/prod/update-ci.sh
+++ cmds/prod/update-ci.sh
@@ -3,8 +3,6 @@
 EXT_IP=130.211.143.253
 FILENAME=repoz-web/target/repoz-web.war
 
-ls -la ~/.ssh
+scp -i ~/.ssh/repozci -o ConnectTimeout=15 -o UserKnownHostsFile=/dev/null -o CheckHostIP=no -o StrictHostKeyChecking=no "$FILENAME" "repoz@$EXT_IP:packs/repoz.war"
 
-scp -v -i ~/.ssh/repozci -o ConnectTimeout=15 -o UserKnownHostsFile=/dev/null -o CheckHostIP=no -o StrictHostKeyChecking=no "$FILENAME" "repoz@$EXT_IP:packs/repoz.war"
-
-ssh -v -i ~/.ssh/repozci -o ConnectTimeout=15 -o UserKnownHostsFile=/dev/null -o CheckHostIP=no -o StrictHostKeyChecking=no "repoz@$EXT_IP" ./cmds/prod/repoz-install/repoz-update.sh
+ssh -i ~/.ssh/repozci -o ConnectTimeout=15 -o UserKnownHostsFile=/dev/null -o CheckHostIP=no -o StrictHostKeyChecking=no "repoz@$EXT_IP" ./cmds/prod/repoz-install/repoz-update.sh
