58179a70c0ec30e98669eadff1018a58e7ea9001
murer <murer@dextra-sw.com>
2016-02-26 18:25:11 -0300

    docker

--- cmds/prod/update-ci.sh
+++ cmds/prod/update-ci.sh
@@ -3,6 +3,6 @@
 EXT_IP=130.211.143.253
 FILENAME=repoz-web/target/repoz-web.war
 
-scp -i ~/.ssh/repozci -o ConnectTimeout=15 -o UserKnownHostsFile=/dev/null -o CheckHostIP=no -o StrictHostKeyChecking=no "$FILENAME" "repoz@$EXT_IP:packs/repoz.war"
+sudo scp -i ~/.ssh/repozci -o ConnectTimeout=15 -o UserKnownHostsFile=/dev/null -o CheckHostIP=no -o StrictHostKeyChecking=no "$FILENAME" "repoz@$EXT_IP:packs/repoz.war"
 
-ssh -i ~/.ssh/repozci -o ConnectTimeout=15 -o UserKnownHostsFile=/dev/null -o CheckHostIP=no -o StrictHostKeyChecking=no "repoz@$EXT_IP" ./cmds/prod/repoz-install/repoz-update.sh
+sudo ssh -i ~/.ssh/repozci -o ConnectTimeout=15 -o UserKnownHostsFile=/dev/null -o CheckHostIP=no -o StrictHostKeyChecking=no "repoz@$EXT_IP" ./cmds/prod/repoz-install/repoz-update.sh
