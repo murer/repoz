6c5e378df0935ac8deb697d1512d053877aa5a82
murer <murer@dextra-sw.com>
2016-02-26 15:13:40 -0300

    docker

--- cmds/prod/logs.sh
+++ cmds/prod/logs.sh
@@ -5,4 +5,5 @@ EXT_IP=130.211.143.253
 #	EXT_IP="$(gcutil --project cloudcontainerz listinstances  --zone us-central1-a  --filter="name eq 'repoz'" | cut -d"|" -f6 | tail -n +4 | head -n 1| sed "s/\s\+//g")"
 #done
 
+
 gcloud compute ssh repoz --command "sudo find /home/repoz -name '*.log' | xargs tail -n 100 -f"
