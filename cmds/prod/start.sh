dc6823520c5324a51fdeb7d1c4204d12991cacfd
murer <murer@dextra-sw.com>
2016-02-26 16:57:19 -0300

    send redirect

--- cmds/prod/start.sh
+++ cmds/prod/start.sh
@@ -7,9 +7,7 @@ gcloud compute instances create repoz \
     --machine-type n1-standard-1 \
     --image debian \
     --address 130.211.143.253 \
-		--metadata-from-file \
-		    startup-script=cmds/prod/repoz-install/install-instance.sh \
-				repoz-update=cmds/prod/repoz-install/repoz-update.sh \
+		--metadata-from-file 'startup-script=cmds/prod/repoz-install/install-instance.sh,repoz-update=cmds/prod/repoz-install/repoz-update.sh' \
     --scopes 'https://www.googleapis.com/auth/devstorage.read_only'
 
 while ! gcloud compute ssh repoz --project cloudcontainerz --zone us-central1-a --command whoami; do sleep 2; done
