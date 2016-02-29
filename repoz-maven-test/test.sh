48bcca2ddb8e830d114d2440b8858b5220f3b98c
murer <murer@dextra-sw.com>
2016-02-29 14:50:38 -0300

    docker

--- repoz-maven-test/test.sh
+++ repoz-maven-test/test.sh
@@ -1,20 +1,34 @@
 #!/bin/bash -xe
 
-#rm -rf gen/repository || true
-mkdir -p gen/files || true
-
+rm -rf gen/maven || true
+mkdir -p gen/files mkdir -p gen/maven || true
 ls gen/files/appengine-tools-sdk-1.9.32.jar || wget 'http://repo1.maven.org/maven2/com/google/appengine/appengine-tools-sdk/1.9.32/appengine-tools-sdk-1.9.32.jar' -O 'gen/files/appengine-tools-sdk-1.9.32.jar'
 
-mvn deploy:deploy-file \
-    -s settings/maven.settings.xml \
-    -DgroupId=com.murerz.repoz.test \
-    -DartifactId=appengine-tools-sdk \
-    -Dversion=1.9.32 \
-    -Dpackaging=jar \
-    -DrepositoryId=repoz \
-    -Durl=http://repoz.dextra.com.br/repozix/r/test/repository \
-    -Dfile=gen/files/appengine-tools-sdk-1.9.32.jar
+BASE_PATH="$PATH"
 
-mvn clean install -s settings/maven.settings.xml -Dmaven.test.skip
+check_version() {
+  ls "gen/files/$2" || wget "$3" -O "gen/files/$2"
+  cd gen/maven
+  tar xzf "../files/$2"
+  cd -
+  export MAVEN_HOME="$PWD/gen/maven/apache-maven-$1"
+  sed "s/gen\/repository/gen\/repository-$1/g" settings/maven.settings.xml > "$MAVEN_HOME/settings.xml"
+  $MAVEN_HOME/bin/mvn -version
 
-mvn deploy -s settings/maven.settings.xml
+  mvn deploy:deploy-file \
+      -s "$MAVEN_HOME/settings.xml" \
+      -DgroupId=com.murerz.repoz.test \
+      -DartifactId=appengine-tools-sdk \
+      -Dversion=1.9.32 \
+      -Dpackaging=jar \
+      -DrepositoryId=repoz \
+      -Durl=http://repoz.dextra.com.br/repozix/r/test/repository \
+      -Dfile=gen/files/appengine-tools-sdk-1.9.32.jar
+
+  mvn clean install -s "$MAVEN_HOME/settings.xml" -Dmaven.test.skip
+
+  mvn deploy -s "$MAVEN_HOME/settings.xml"
+}
+
+check_version '3.2.2' 'apache-maven-3.2.2-bin.tar.gz' 'https://archive.apache.org/dist/maven/binaries/apache-maven-3.2.2-bin.tar.gz'
+check_version '2.2.1' 'apache-maven-2.2.1-bin.tar.gz' 'https://archive.apache.org/dist/maven/binaries/apache-maven-2.2.1-bin.tar.gz'
