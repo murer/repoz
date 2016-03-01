472bf4550572c4ddf95107040a4007d453570a00
murer <murer@dextra-sw.com>
2016-03-01 14:46:53 -0300

    docker

--- cmds/prod/ssl/update-key.sh
+++ cmds/prod/ssl/update-key.sh
@@ -2,8 +2,5 @@
 
 cd target/ssl
 gsutil -m rm -r gs://cz-repoz-config/repoz-repo/ssl
-gsutil -m cp repoz.dextra.com.br.csr repoz.dextra.com.br.key sub.class1.server.ca.pem gs://cz-repoz-config/repoz-repo/ssl
-
-cat repoz.dextra.com.br.csr
-
+gsutil -m cp repoz.dextra.com.br.csr repoz.dextra.com.br.pem repoz.dextra.com.br.key sub.class1.server.ca.pem gs://cz-repoz-config/repoz-repo/ssl
 cd -
